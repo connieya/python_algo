754c82142272aac28487b74133742545011f1ee8
connieya <gunny6026@naver.com>
2023-01-07 14:47:07 +0900

    LeetCode 179. Largest Numberˆ

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-S = "abc"
+a,b,c = 10,20,30
 
-print(S[0])
+print(a,b,c)
+
+print(str(31)+str(9))
+
+print("319" >"331")
+
+arr = [1,32,223,113,3]
+
+res = "".join(map(str,arr))
+
+print(res)
+
+
+arr2 = ["4","23","1333","87"]
+
+res = "".join(arr2)
+
+print(res)
